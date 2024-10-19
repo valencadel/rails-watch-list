e33e53bdaf8dff7d9e40dc043cab3e04bac9b143
Valentin Cadel <104843541+valencadel@users.noreply.github.com>
2024-10-19 18:41:52 -0300

    watch-list con backend funcionando

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path(@bookmark.list), notice: 'Bookmark was successfully deleted.'
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
